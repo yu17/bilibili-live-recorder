6c9648165f70c9972d7f210bc276676b3cf3d11a
yu17 <yjq17@hotmail.com>
2020-08-14 22:33:56 -0400

    Now allows changing saving path in config.py.

--- run.py
+++ run.py
@@ -11,11 +11,12 @@ urllib3.disable_warnings()
 
 
 class BiliBiliLiveRecorder(BiliBiliLive):
-    def __init__(self, room_id, check_interval=5*60):
+    def __init__(self, room_id, check_interval=5*60, saving_path = 'files'):
         super().__init__(room_id)
         self.inform = utils.inform
         self.print = utils.print_log
         self.check_interval = check_interval
+        self.saving_path = saving_path
 
     def check(self, interval):
         while True:
@@ -69,7 +70,11 @@ class BiliBiliLiveRecorder(BiliBiliLive):
             try:
                 urls = self.check(interval=self.check_interval)
                 filename = utils.generate_filename(self.room_id,self.room_info['roomname'])
-                c_filename = os.path.join(os.getcwd(), 'files', filename)
+                if os.path.isabs(saving_path):
+                    c_filename = os.path.join(saving_path, filename)
+                else:
+                    c_filename = os.path.join(os.getcwd(), saving_path, filename)
+                print(c_filename)
                 self.record(urls[0], c_filename, self.check_interval)
                 self.print(self.room_id, '录制完成' + c_filename)
             except Exception as e:
@@ -80,13 +85,15 @@ if __name__ == '__main__':
     print(sys.argv)
     if len(sys.argv) == 2:
         input_id = [str(sys.argv[1])]
+        saving_path = 'files'
     elif len(sys.argv) == 1:
-        input_id = config.rooms  # input_id = '917766' '1075'
+        input_id = config.rooms
+        saving_path = config.default_saving_path
     else:
         raise ZeroDivisionError('请检查输入的命令是否正确 例如：python3 run.py 10086')
 
     mp = multiprocessing.Process
-    tasks = [mp(target=BiliBiliLiveRecorder(room_id).run) for room_id in input_id]
+    tasks = [mp(target=BiliBiliLiveRecorder(room_id, 300, saving_path).run) for room_id in input_id]
     for i in tasks:
         i.start()
     for i in tasks:
